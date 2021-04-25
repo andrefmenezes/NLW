957bb22a07fd39d0c90954a1584ea5de6751d07f
andrefmenezes <decomenezes00@gmail.com>
2021-04-25 12:55:53 -0300

    first commit

---
+++ front/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+//JSX: sintaxe de xml dentro do JS
+import Routes from './routes';
+
+
+function App() {
+  
+  return (
+   <Routes />
+  );
+}
+
+export default App;
---
+++ server/src/routes.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+const routes = express.Router();
+const pointsControllers = new PointsController();
+const itemsControllers = new ItemsController();
+routes.get('/items',itemsControllers.index);
+routes.post('/points',pointsControllers.create );
+routes.get('/points/:id',pointsControllers.show );
+routes.get('/points',pointsControllers.index );
+
+    export default routes;
